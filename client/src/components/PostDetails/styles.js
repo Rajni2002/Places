534c738018070f4a6ba41d9797c003cca3c65f54
Rajni2002 <rajnikantdash12@gmail.com>
2022-07-10 14:01:16 +0530

    Post details Setted up

---
+++ client/src/components/PostDetails/styles.js
@@ -0,0 +1,52 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+  media: {
+    borderRadius: "20px",
+    objectFit: "cover",
+    width: "100%",
+    maxHeight: "600px",
+  },
+  card: {
+    display: "flex",
+    width: "100%",
+    "@media (max-width: 600px)": {
+      flexWrap: "wrap",
+      flexDirection: "column",
+    },
+  },
+  section: {
+    borderRadius: "20px",
+    margin: "10px",
+    flex: 1,
+  },
+  imageSection: {
+    marginLeft: "20px",
+    "@media (max-width: 600px)": {
+      marginLeft: 0,
+    },
+  },
+  recommendedPosts: {
+    display: "flex",
+    "@media (max-width: 600px)": {
+      flexDirection: "column",
+    },
+  },
+  loadingPaper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "20px",
+    borderRadius: "15px",
+    height: "39vh",
+  },
+  commentsOuterContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  commentsInnerContainer: {
+    height: "200px",
+    overflowY: "auto",
+    marginRight: "30px",
+  },
+}));
